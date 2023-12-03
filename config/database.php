321cee60a29d236cf80716681930d7f83ee5a096
dormidosan <metahuargen@gmail.com>
2023-12-03 22:57:13 +0000

    login working

--- config/database.php
+++ config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => array_key_exists('REDIS_CLIENT', $_SERVER)? $_SERVER['REDIS_CLIENT']: env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -130,7 +130,7 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => array_key_exists('AWS_REDIS_HOST', $_SERVER)? $_SERVER['AWS_REDIS_HOST']: env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
